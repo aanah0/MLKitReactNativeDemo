316c78217b520619fb854a6ddf0785016b75e8fe
Nick Popov <marketing.popov@gmail.com>
2023-02-21 12:20:27 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'light-content'} />
+    </SafeAreaView>
+  );
+}
+
+export default App;
